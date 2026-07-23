495fe61cc4e8bb7aa745cc5d569e6e7541566739
Anthony Mendez <anthonymendez9@gmail.com>
2026-07-22 20:04:56 -0700

    fix: implement ClientModInitializer and update build configuration for Fabric support

--- src/main/java/me/crafter/mc/lockettepro/client/LocketteProClientMod.java
+++ src/main/java/me/crafter/mc/lockettepro/client/LocketteProClientMod.java
@@ -1,15 +1,17 @@
 package me.crafter.mc.lockettepro.client;
 
 import me.crafter.mc.lockettepro.LocketteProNetwork;
+import net.fabricmc.api.ClientModInitializer;
 
 /**
  * Client-side Fabric Mod initializer for LocketteProMax.
  * Entrypoint referenced in fabric.mod.json when launched in a Fabric client environment.
  */
-public class LocketteProClientMod {
+public class LocketteProClientMod implements ClientModInitializer {
 
   public static final String MOD_ID = "lockettepromax";
 
+  @Override
   public void onInitializeClient() {
     System.out.println("[LocketteProMax] Initializing Client Fabric Mod...");
     // Register client-side network payload listener and keybind handlers when running on Fabric Client
